d3ad2036473458c016da987674bc0589d21d3734
Гость <Guest@mac-mini-jenkins.local>
2017-06-20 10:46:03 +0300

    Initial Commit

---
+++ Vocabulary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vocabulary
+//
+//  Created by Гость on 20.06.17.
+//  Copyright © 2017 guestOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
